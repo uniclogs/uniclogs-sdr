3be8cee4fa06c6289b084b68b690a8cbe67ca87f
Glenn N. LeBrasseur <glenn.lebrasseur@gmail.com>
2021-08-04 17:18:22 -0700

    Minor updates to g3ruh transceiver

--- flowgraphs/send_ax25_packet.sh
+++ flowgraphs/send_ax25_packet.sh
@@ -14,3 +14,11 @@
 #  Payload:        Hi
 #
 echo "a682a84040406096946ea6aa406103004869" | xxd -r -p | nc localhost 52002
+#
+#
+#  NB: To setup up the server to listen to the Rx socket the following could
+#      be submitted at the sommand line before the flowgraph is started:
+#
+#  $ nc -l localhost 52001 | tee data.out | hexdump -C
+#
+#
